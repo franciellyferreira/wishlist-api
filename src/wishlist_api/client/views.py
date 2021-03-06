5acaa22abd0035cb4d32088cf1350a6439998639
francielly.silva <francielly.silva@luizalabs.com>
2021-03-06 10:26:04 -0300

    feat: create route list clients with pagination

--- src/wishlist_api/client/views.py
+++ src/wishlist_api/client/views.py
@@ -1,13 +1,34 @@
+from http import HTTPStatus
+
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from wishlist_api.client.models import Client
 from wishlist_api.client.serializers import ClientSerializer
+from wishlist_api.pagination import CustomPagination
 
 
-class ClientList(APIView):
+class ClientList(GenericAPIView):
+    serializer_class = ClientSerializer
+    pagination_class = CustomPagination
+    queryset = Client.objects.all().order_by('name')
 
-    def get(self, request, format=None):
-        clients = Client.objects.all()
-        serializer = ClientSerializer(clients, many=True)
-        return Response(serializer.data)
+    def get(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            result = self.get_paginated_response(serializer.data)
+            data = result.data
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            data = serializer.data
+
+        payload = {
+            'status_code': HTTPStatus.OK.value,
+            'message': 'success',
+            'data': data
+        }
+
+        return Response(payload)
